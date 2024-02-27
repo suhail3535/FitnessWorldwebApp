85fc52d86a17d21be73255426b1f4857bfbdf443
suhail khan <khansohail015@gmail.com>
2024-02-27 18:50:20 +0530

    update mentors page

--- src/Pages/trainers/Trainers.jsx
+++ src/Pages/trainers/Trainers.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import "./trainer.css"
-import { trainers } from "../../data.js"
+import React from "react";
+import "./trainer.css";
+import { trainers } from "../../data.js";
 const Trainers = () => {
   return (
     <div className="trainer_div">
-      <h1 style={{ marginBottom: "2rem", textAlign: "center" }}>Meet Our Trainers</h1>
+      <h1 style={{ marginBottom: "2rem", textAlign: "center" }}>
+        Meet Our Trainers
+      </h1>
       <div id="trainer_details_div">
-        {trainers.map((item,index) => {
-          return <div key={index}>
-            <img src={item.image} alt="img" />
-            <h2>{item.name}</h2>
-            <h5>{item.job}</h5>
+        {trainers.map((item, index) => {
+          return (
+            <div  className="trainer_card" key={index}>
+              <div className="trainer_image">
 
-          </div>
+                <img src={item.image} alt="img" />
+              </div>
+              <h2>{item.name}</h2>
+              <h5>{item.job}</h5>
+            </div>
+          );
         })}
+      </div>
     </div>
+  );
+};
 
-
-
-
-    </div>
-  )
-}
-
-export default Trainers
+export default Trainers;
