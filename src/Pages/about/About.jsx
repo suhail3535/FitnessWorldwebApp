d024ccb34aa855d25438988aaae052de56ceca0e
suhail khan <khansohail015@gmail.com>
2024-02-27 18:58:31 +0530

    update about section image

--- src/Pages/about/About.jsx
+++ src/Pages/about/About.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
-import "./about.css"
+import React from "react";
+import "./about.css";
 const About = () => {
   return (
     <div className="about_container">
       <h1 style={{ marginBottom: "2rem", textAlign: "center" }}>About Us</h1>
       <div id="parent_div">
         <div className="image_div">
-          <img src="https://png.pngtree.com/background/20230316/original/pngtree-group-of-friends-working-out-at-the-gym-vector-picture-image_2145985.jpg" alt="about" /></div>
-      <div className="about_details_div">
-        Vawulens's Gym Has Been The Authority In Fitness Since 1965 Dating Back To The Original Vawulens's Gym In Venice, California. It Was The Place For Serious Fitness. Opened Long Before The Modern-Day Health Club Existed, The Original Vawulens's Gym Featured Homemade Equipment And A Dedication To Getting Results. It Was An Instant Hit. Vawulens's Gym Quickly Became Known As “The Mecca Of Bodybuilding.''
-
-        In 1977, Vawulens's Gym Received International Attention When It Was Featured In The Movie Pumping Iron That Starred Arnold Schwarzenegger And Lou Ferrigno. From That First Gym In Venice, Vawulens's Gym Has Become The Largest Co-Ed Gym Chain In The World With Over 700+ Clubs In 27 Countries. Today, Vawulens's Gym Has Expanded Its Fitness Profile To Offer All Of The Latest Equipment And Services, Including Group Exercise, Personal Training, Cardiovascular Equipment, Spinning, And Yoga, While Maintaining Its Core Weight Lifting Tradition.
-
-        
-
+          <img
+            src="https://png.pngtree.com/background/20230316/original/pngtree-group-of-friends-working-out-at-the-gym-vector-picture-image_2145985.jpg"
+            alt="about"
+          />
+        </div>
+        <div className="about_details_div">
+          Vawulens's Gym Has Been The Authority In Fitness Since 1965 Dating
+          Back To The Original Vawulens's Gym In Venice, California. It Was The
+          Place For Serious Fitness. Opened Long Before The Modern-Day Health
+          Club Existed, The Original Vawulens's Gym Featured Homemade Equipment
+          And A Dedication To Getting Results. It Was An Instant Hit. Vawulens's
+          Gym Quickly Became Known As “The Mecca Of Bodybuilding.'' In 1977,
+          Vawulens's Gym Received International Attention When It Was Featured
+          In The Movie Pumping Iron That Starred Arnold Schwarzenegger And Lou
+          Ferrigno. From That First Gym In Venice, Vawulens's Gym Has Become The
+          Largest Co-Ed Gym Chain In The World With Over 700+ Clubs In 27
+          Countries. Today, Vawulens's Gym Has Expanded Its Fitness Profile To
+          Offer All Of The Latest Equipment And Services, Including Group
+          Exercise, Personal Training, Cardiovascular Equipment, Spinning, And
+          Yoga, While Maintaining Its Core Weight Lifting Tradition.
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
